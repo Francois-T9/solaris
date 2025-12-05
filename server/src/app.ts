7f68216bbb1207336529245984b11cf2eb867830
Francois-T9 <francois.thullier98@gmail.com>
2025-12-05 16:13:52 +0100

    removed credentials from cors settings

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,6 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
